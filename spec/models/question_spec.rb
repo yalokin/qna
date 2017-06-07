9e5e852dc9749deb20502dd3f4dc63b7935c9c6a
Nikolay Chumin <chumin.n@gmail.com>
2017-06-07 08:59:27 +0300

    delete match for user_name

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,5 +5,4 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should validate_presence_of :user_name }
 end
