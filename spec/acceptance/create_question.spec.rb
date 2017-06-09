769cc6a877141b5bf71b09d98021405e4c21e849
Nikolay Chumin <chumin.n@gmail.com>
2017-06-09 13:00:14 +0300

    add acceptance test for create question

---
+++ spec/acceptance/create_question.spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Create question', %q{
+  In order ti get answer from community
+  As any user
+  I want to be abble to ask question
+} do
+
+  scenario 'User create the question' do
+    visit new_question_path
+    fill_in 'Title', with: 'Title of question'
+    fill_in 'Text', with: 'Text of question'
+    click_on 'Create'
+
+    expect(page).to have_content 'Title of question'
+    expect(page).to have_content 'Text of question'
+  end
+end
