33dfc203ed78d9d5dc73985d52969dce9a6faf2b
Nikolay Chumin <chumin.n@gmail.com>
2017-06-10 13:41:59 +0300

    fix lazy load

--- spec/acceptance/user_can_watch_questions.rb
+++ spec/acceptance/user_can_watch_questions.rb
@@ -5,10 +5,9 @@ In order to be able to view questions
 as a user
 } do
 
-  given(:questions) { create_list(:question, 2) }
+  given!(:questions) { create_list(:question, 2) }
 
   scenario 'User view questions' do
-    questions
     visit questions_path
     questions.each do |question|
       expect(page).to have_content question.title
