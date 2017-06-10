85a5cc07c768c7ac7452f87edde6c3c0681f55d1
Nikolay Chumin <chumin.n@gmail.com>
2017-06-10 13:36:13 +0300

    add history -  user can watch questions

---
+++ spec/acceptance/user_can_watch_questions.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can watch the questions', %q{
+In order to be able to view questions
+as a user
+} do
+
+  given(:questions) { create_list(:question, 2) }
+
+  scenario 'User view questions' do
+    questions
+    visit questions_path
+    questions.each do |question|
+      expect(page).to have_content question.title
+    end
+  end
+end
