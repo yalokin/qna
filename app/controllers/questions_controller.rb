9f64df35b752fd8783c6d6cb882640115ac8521e
Nikolay Chumin <chumin.n@gmail.com>
2017-06-11 13:53:47 +0300

    Added user answer for question history

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,6 +19,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @answer = Answer.new
   end
 
   private
--- spec/acceptance/create_answer_for_question_spec.rb
+++ spec/acceptance/create_answer_for_question_spec.rb
@@ -1,1 +1,16 @@
-scenario "Create answer for question"
+require 'rails_helper'
+
+feature 'Create answer for question', %q{
+  To be abble to create answers for question
+} do
+  given(:question) { create :question }
+
+  scenario 'User can to create answer' do
+    visit question_path(question)
+
+    fill_in 'Your answer', with: 'Answer text'
+    click_on 'Answer'
+
+    expect(page).to have_content 'Answer text'
+  end
+end
