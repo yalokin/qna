4834b736bf2fb42596dcd7642a8cd93ad367a4b6
Nikolay Chumin <chumin.n@gmail.com>
2017-06-11 15:39:31 +0300

    Added user can watch question and ansers to it history

---
+++ spec/acceptance/user_can_watch_question_and_answers_to_it_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can watch question with answer', %q{
+  To look at the question with the answers
+} do
+
+  given!(:question) { create :question }
+  given!(:answers) { create_list(:answer, 2, question: question) }
+
+  scenario 'View question page with answers' do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+end
+
