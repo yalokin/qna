3b7f1357ac9cbc439460aba8f0ff2e7b5baba49d
Nikolay Chumin <chumin.n@gmail.com>
2017-06-12 13:04:44 +0300

    Added user sign in history

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -1,1 +1,31 @@
-scenario 'User can sign in'
+require 'rails_helper'
+
+feature 'User sign in', %q{
+  In order to abble to ask question
+  As an user
+  I want to be able to sign in
+} do
+
+  scenario 'Registered user try to sign in' do
+    User.create!(email: 'user@test.ru', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.ru'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Non-registered user try to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.ru'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+    expect(current_path).to eq new_user_session_path
+  end
+
+end
