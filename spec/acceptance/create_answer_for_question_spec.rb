7233fe9f019fe078bce7a1fa4dc5247c00732fe7
Nikolay Chumin <chumin.n@gmail.com>
2017-06-13 00:55:34 +0300

    Added auth to create answer for question

--- spec/acceptance/create_answer_for_question_spec.rb
+++ spec/acceptance/create_answer_for_question_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 feature 'Create answer for question', %q{
   To be abble to create answers for question
 } do
+  given(:user) { create :user }
   given(:question) { create :question }
 
-  scenario 'User can to create answer' do
+  scenario 'Auth user can to create answer' do
+    sign_in(user)
+
     visit question_path(question)
 
     fill_in 'Your answer', with: 'Answer text'
