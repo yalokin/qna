705e3493809d3b76025388e1b6ab07cdbda646ab
Nikolay Chumin <chumin.n@gmail.com>
2017-06-15 17:22:55 +0300

    added test associate question with user

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -21,8 +21,8 @@ RSpec.describe QuestionsController, type: :controller do
     sign_in_user
 
     context 'with valid attributes' do
-      it 'saves the new question in the database' do
-        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      it 'assign new question with user and save' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(@user.questions, :count).by(1)
       end
 
       it 'redirects to new view' do
