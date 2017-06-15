e1bf36c2c0e01e9424f13eee0deea0e96f47e620
Nikolay Chumin <chumin.n@gmail.com>
2017-06-15 18:55:30 +0300

    add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe User do
+RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it { should have_many(:answers) }
+  it { should have_many(:questions) }
+
+  let(:user) { create :user }
+  let(:question) { create(:question, user: user) }
+  let(:user_without_question) { create :user }
+
+  describe 'author_of? method' do
+    it 'return true if uesr athothor of resource' do
+      expect(user.author_of?(question)).to be true
+    end
+
+    it 'return false if user is not author of resource' do
+      expect(user_without_question.author_of?(question)).to be false
+    end
+  end
 end
