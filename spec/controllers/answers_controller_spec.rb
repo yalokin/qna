9265cb19f2e376852b56516a1c87c6117f120196
Nikolay Chumin <chumin.n@gmail.com>
2017-06-15 16:14:56 +0300

    Added check assotiation and refactoring

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,27 +5,31 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     sign_in_user
+    let(:params) { { answer: attributes_for(:answer), question_id: question} }
+    let(:invalid_params) { { answer: attributes_for(:invalid_answer), question_id: question } }
 
     context 'with valid attributes' do
       it 'saves new answer in database' do
-        params = { answer: attributes_for(:answer), question_id: question }
         expect { post :create, params: params }.to change(question.answers, :count).by(1)
       end
 
+      it 'assign new answer with user' do
+        expect { post :create, params: params }.to change(@user.answers, :count).by(1)
+      end
+
       it 'redirects to show view' do
-        post :create, params: {answer: attributes_for(:answer), question_id: question}
+        post :create, params: params
         expect(response).to redirect_to assigns(:question)
       end
     end
 
-    context  'with invalid attributes' do
+    context 'with invalid attributes' do
       it 'does not save the answer' do
-        params = { answer: attributes_for(:invalid_answer), question_id: question }
-        expect { post :create, params: params }.to_not change(Answer, :count)
+        expect { post :create, params: invalid_params }.to_not change(Answer, :count)
       end
 
       it 're-renders new view' do
-        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
+        post :create, params: invalid_params
         expect(response).to render_template 'questions/show'
       end
 
@@ -39,7 +43,7 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'author delete answer' do
       it 'delete answer' do
-        expect { delete :destroy, params: {id: answer_user } }.to change(Answer, :count).by(-1)
+        expect { delete :destroy, params: { id: answer_user } }.to change(Answer, :count).by(-1)
       end
 
       it 'redirect to question' do
