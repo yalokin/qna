0b72c14af3be6de4962425b928f5b86d8fbf437f
Nikolay Chumin <chumin.n@gmail.com>
2017-06-16 18:45:31 +0300

    added foreign key

--- db/migrate/20170613170043_add_questions_to_user.rb
+++ db/migrate/20170613170043_add_questions_to_user.rb
@@ -1,5 +1,5 @@
 class AddQuestionsToUser < ActiveRecord::Migration[5.1]
   def change
-    add_belongs_to :questions, :user, index: true
+    add_belongs_to :questions, :user, foreign_key: true
   end
 end
