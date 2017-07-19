c7c3bef86d66aa4f615d6d2ca854815714acec78
Nikolay Chumin <chumin.n@gmail.com>
2017-07-19 18:13:00 +0300

    move attachable to concern

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
-
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachable
 
   validates :title, :body, presence: true
 
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank
+  include Attachable
 end
