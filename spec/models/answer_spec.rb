5bcfb3db4e7adf2461c1ea335722f37d70758320
Nikolay Chumin <chumin.n@gmail.com>
2017-07-27 12:43:26 +0300

    add models tests for votable

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative 'concerns/votable_spec.rb'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
@@ -9,6 +10,8 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
 
   it { should accept_nested_attributes_for :attachments }
+
+  it_behaves_like 'votable'
 
   let(:question) { create :question }
   let!(:answer) { create :best_answer, question: question }
@@ -33,4 +36,5 @@ RSpec.describe Answer, type: :model do
       end
     end
   end
+
 end
---
+++ spec/models/concerns/votable_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.shared_examples 'votable' do
+  let(:model) { described_class }
+  let(:votable) { create(model.to_s.underscore.to_sym) }
+  let(:user) { create(:user) }
+
+  describe '#vote_up' do
+    it 'should set vote +1 to votable' do
+      votable.vote(user, 'up')
+      expect(Vote.last.value).to eq 1
+    end
+
+    it 'should increment votable rating' do
+      votable.vote(user, 'up')
+      expect(votable.rating).to eq 1
+    end
+  end
+
+  describe '#vote_down' do
+    it 'should set vote -1 to votable' do
+      votable.vote(user, 'down')
+      expect(Vote.last.value).to eq -1
+    end
+
+    it 'should decrement votable rating' do
+      votable.vote(user, 'down')
+      expect(votable.rating).to eq -1
+    end
+  end
+end
